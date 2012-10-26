3a946a338e3ea2329cebd1aa4e26dddae795d400
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-10-25 22:04:13 -0400

    Created header for cowboy and cut out example cruft.

--- ext/cowboy/cowboy.c
+++ ext/cowboy/cowboy.c
@@ -1,10 +1,7 @@
-#include <ruby.h>
-
-static VALUE hello_world(VALUE mod){
-  return rb_str_new2("hello world");
-}
+#include <cowboy.h>
 
 void Init_cowboy(){
   VALUE mCowboy = rb_define_module("Cowboy");
-  rb_define_singleton_method(mCowboy, "hello_world", hello_world, 0);
+
+  Init_cowboy_complex();
 }
---
+++ ext/cowboy/cowboy.h
@@ -0,0 +1,11 @@
+#ifndef RUBY_COWBOY
+#define RUBY_COWBOY
+
+#include <ruby.h>;
+#include <fftw3.h>;
+
+#include <cowboy_complex.h>;
+
+extern VALUE mCowboy;
+
+#endif
