af8415687c38cd1231aeb2dfce54a55216c3016d
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-10-25 21:56:15 -0400

    Checks for library correctly.

--- ext/cowboy/extconf.rb
+++ ext/cowboy/extconf.rb
@@ -1,2 +1,29 @@
 require 'mkmf'
 create_makefile('cowboy/cowboy')
+
+LIBDIR = RbConfig::CONFIG['libdir']
+INCLUDEDIR = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+              '/opt/local/include',
+              '/usr/local/include',
+              INCLUDEDIR,
+              '/usr/include',
+]
+
+LIB_DIRS = [
+            '/opt/local/lib',
+            '/usr/local/lib',
+            LIBDIR,
+            '/usr/lib',
+]
+
+dir_config('fftw', HEADER_DIRS, LIB_DIRS)
+
+unless find_header('fftw3.h')
+  abort "FFTw is missing, please install FFTw"
+end
+
+unless find_library('fftw3', "fftw_malloc")
+  abort "FFTw is missing, please install FFTw"
+end
