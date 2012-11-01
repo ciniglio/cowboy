6684ffd77a7f61074d5ef709eb091a971dcf984e
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-11-01 14:30:03 -0400

    Add frequencies class to do shift and include frequency buckets.

--- lib/cowboy/fft.rb
+++ lib/cowboy/fft.rb
@@ -1,4 +1,32 @@
 module Cowboy
+  class Frequencies
+    attr_reader :dc, :unshifted, :freq
+
+    def initialize(unshifted_array)
+      @unshifted = unshifted_array
+      @dc = unshifted_array[0].real
+      n = unshifted_array.size
+      @freq = []
+      for i in (n/2...n)
+        @freq << unshifted_array[i].abs
+      end
+      for i in (0...n/2)
+        @freq << unshifted_array[i].abs
+      end
+    end
+
+    def buckets(sample_rate=nil)
+      sample_rate ||= @freq.size
+      nyquist = sample_rate/2
+      a = []
+      n = @freq.size
+      for i in (0..n)
+        a << -nyquist + (2 * nyquist/n) * i
+      end
+      return a
+    end
+  end
+
   def Cowboy::fft(arr, w=Hamming, n=29)
     if !w.nil?
       window = w.new(n)
