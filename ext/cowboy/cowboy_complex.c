a6ac26b7764267adf3e27c79e1ef7c12b7487ec6
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-11-05 18:19:32 -0500

    Code cleanup

--- ext/cowboy/cowboy_complex.c
+++ ext/cowboy/cowboy_complex.c
@@ -58,24 +58,5 @@ VALUE c_to_rb_complex(double r, double i){
                     DBL2NUM(r), DBL2NUM(i));
 }
 
-VALUE complex_to_real_nums(fftw_complex *fc, long N){
-  VALUE ar = rb_ary_new2(N);
-  int i;
-  for(i = 0; i < N; i++){
-    rb_ary_store(ar, i, c_to_rb_complex(fc[i][0],
-                                        fc[i][1]));
-  }
-  return ar;
-}
-
-VALUE test_back_and_from_complex(VALUE m, VALUE nums){
-  fftw_complex * fc;
-  Check_Type(nums, T_ARRAY);
-  fc = allocate_fftw_complex(size_of_ary(nums));
-  cast_nums_to_complex(fc, nums);
-  return complex_to_real_nums(fc,
-                              size_of_ary(nums));
-}
-
 void Init_cowboy_complex(){
 }
--- ext/cowboy/cowboy_complex.h
+++ ext/cowboy/cowboy_complex.h
@@ -8,7 +8,6 @@ void Init_cowboy_complex();
 void cast_nums_to_complex(fftw_complex * fc, VALUE nums);
 void cast_string_to_complex(fftw_complex * fc, VALUE nums);
 fftw_complex * allocate_fftw_complex(long n);
-VALUE complex_to_real_nums(fftw_complex * fc, long N);
 long size_of_ary(VALUE nums);
 long size_of_str(VALUE str);
 VALUE c_to_rb_complex(double r, double i);
