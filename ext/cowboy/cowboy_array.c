a3db4e6484c33858c679081d41d970be3bf9d7c2
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-11-06 11:52:26 -0500

    CowboyArray is now Enumerable!
    This means we get things like select and inject for free.

--- ext/cowboy/cowboy_array.c
+++ ext/cowboy/cowboy_array.c
@@ -25,6 +25,28 @@ VALUE array_index(VALUE self, VALUE index){
                          ca->fc[i][1]);
 }
 
+VALUE array_size(VALUE self){
+  fftw_complex * fc;
+  CowboyArray * ca;
+  Data_Get_Struct(self, CowboyArray, ca);
+  return LONG2NUM(ca->N);
+}
+
+VALUE array_each(VALUE self){
+  int i;
+  fftw_complex * fc;
+  CowboyArray * ca;
+  Data_Get_Struct(self, CowboyArray, ca);
+
+  if (!rb_block_given_p())
+    rb_raise(rb_eArgError, "Expected Block");
+
+  for(i = 0; i < ca->N; i++)
+    rb_yield(c_to_rb_complex(ca->fc[i][0],
+                             ca->fc[i][1]));
+  return self;
+}
+
 VALUE ca_wrap_struct_class(fftw_complex *fc, long N){
   CowboyArray * ca;
   ca = ALLOC(CowboyArray);
@@ -40,5 +62,8 @@ VALUE ca_not_implemented(VALUE self){
 void Init_cowboy_array(){
   cCowboyArray = rb_define_class_under(mCowboy, "CowboyArray", rb_cObject);
   rb_define_method(cCowboyArray, "initialize", ca_not_implemented, 0);
+  rb_define_method(cCowboyArray, "size", array_size, 0);
   rb_define_method(cCowboyArray, "[]", array_index, 1);
+  rb_define_method(cCowboyArray, "each", array_each, 0);
+  rb_include_module(cCowboyArray, rb_mEnumerable);
 }
---
+++ test/test_enumerable.rb
@@ -0,0 +1,26 @@
+require_relative './test_helper.rb'
+
+class TestEnumerable < Test::Unit::TestCase
+  def test_size
+    a = (0...10000).to_a
+    c = Cowboy::fft_1d a
+    assert_equal c.size, a.size
+  end
+
+  def test_each
+    a = (0...4).to_a
+    c = Cowboy::fft_1d a
+    assert_equal (c.respond_to? :each), true
+    assert_block do
+      c.each do |i|
+        assert_not_nil i
+      end
+    end
+  end
+
+  def test_map
+    a = (0...10).to_a
+    c = Cowboy::fft_1d a
+    assert_equal (c.respond_to? :map), true
+  end
+end
